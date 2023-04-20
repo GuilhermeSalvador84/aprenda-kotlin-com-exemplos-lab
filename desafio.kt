3221252dff7c8d8aca9bc04c090d0430b4663c72
Guilherme Salvador <115181803+GuilhermeSalvador84@users.noreply.github.com>
2023-04-20 13:13:59 -0300

    Final_Desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,63 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Aluno(val nome: String, val matricula: String) {
+        override fun toString(): String {
+            return "$nome - matricula: $matricula"
+        }
+    }
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60) {
+        override fun toString(): String {
+            return "$nome: duracao $duracao horas"
+        }
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val inscritos = mutableListOf<Aluno>()
+
+    fun matricular(vararg alunos: Aluno){
+        inscritos.addAll(alunos)
+    }
+
+    override fun toString(): String{
+        return """
+            Nome: $nome
+            Nivel: $nivel
+            Conteudo Educacional: $conteudos
+            Alunos inscritos: $inscritos
+            
+        """.trimIndent()
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val ven = Aluno("Venilton", "012345")
+    val gui = Aluno("Guilherme", "678910")
+
+    val kotlin = ConteudoEducacional("Kotlin", 117)
+    val java = ConteudoEducacional("Java", 150)
+    val ed = ConteudoEducacional("Estrutura de Dados", 130)
+    val bd = ConteudoEducacional("Banco de Dados", 90)
+    val logicaProgramacao = ConteudoEducacional("Logica de Programacao", 120)
+
+    val desenvolvedorWeb = Formacao(
+        "Desenvolvedor Web",
+        Nivel.BASICO,
+        listOf(logicaProgramacao, ed,kotlin)
+    )
+
+    val desenvolvedorBackEnd = Formacao(
+        "Desenvolvedor BackEnd",
+        Nivel.AVANCADO,
+        listOf(logicaProgramacao, ed, bd, java)
+    )
+
+    desenvolvedorWeb.matricular(ven)
+    desenvolvedorBackEnd.matricular(gui)
+
+    println(desenvolvedorWeb)
+    println(desenvolvedorBackEnd)
 }
